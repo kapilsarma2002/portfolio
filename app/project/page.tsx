a918b8cdd64705c10f30e537493e7674ad1373bf
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-07-14 09:40:27 +0530

    projects

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,77 +1,49 @@
 import Navbar from '@/components/Navbar'
 import ProjectCard from '@/components/ProjectCard'
+import github from '@/public/github.png'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const MainProject = {
   title: '',
   link: '',
   description: '',
   techstack: '',
-  date: ''
+  date: '',
 }
 
 const Projects = [
   {
-    title: 'title1',
-    link: 'link1',
-    date: 'date1',
-    description: 'desc1',
-    techstack: 'tech1',
+    title: 'Docs Clone',
+    link: 'https://github.com/kapilsarma2002/docs-clone',
+    date: 'May-23',
+    description:
+      'This project is a clone of google docs, it has been built using React, TS and Tailwind CSS and deployed to Vercel. The Skeloton of the page is built using Flexbox. It consists of three components Titlebar, Sidebar and Editor',
+    techstack: 'React, Tailwind, draft-js',
   },
   {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
+    title: 'Post Share',
+    link: 'https://github.com/kapilsarma2002/post-share',
+    date: 'Mar-23',
+    description:
+      'A post sharing app which lets users share their thoughts in posts. This was my first experience using a complete framework like angular for frontend and using .Net as well',
+    techstack: 'Angular, .Net, SQL Server',
   },
   {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
+    title: 'Blog website',
+    link: 'https://github.com/kapilsarma2002/blog',
+    date: 'Oct-22',
+    description:
+      "A Simple bloggin website built using nextjs 12 which lets the users write their blogs and also lets the users read others' blogs",
+    techstack: 'Nextjs 12 , React, Chakra UI, Prisma',
   },
   {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
-  },
-  {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
-  },
-  {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
-  },
-  {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
-  },
-  {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
-  },
-  {
-    title: 'title2',
-    link: 'link2',
-    date: 'date2',
-    description: 'desc2',
-    techstack: 'tech2',
+    title: 'Password Manager',
+    link: 'https://github.com/kapilsarma2002/passman-fullst-',
+    date: 'Sept-22',
+    description:
+      'A password manager built using nextjs 12 which lets users store the passwords for different websites. The passwords are encrypted using bcrypt.',
+    techstack: 'Nextjs 12, React, Chakra UI, Prisma',
   },
 ]
 
@@ -79,11 +51,23 @@ const Project = () => {
   return (
     <div className="h-screen w-screen">
       <Navbar />
-      <button className='h-3 w-4 bg-grey text-white'>Add</button>
-      <div className="h-[calc(100%-4rem)] grid grid-cols-2 gap-12 place-items-center mx-40 overflow-y-auto py-10">
+      <div className="h-[calc(100%-4rem)] grid grid-cols-2 gap-24 place-items-center mx-40 overflow-y-auto py-10">
         {Projects.map((project) => (
           <ProjectCard key={project.title} props={project} />
         ))}
+      </div>
+
+      <div className="text-2xl w-full ">
+        <Link
+          href="https://github.com/kapilsarma2002"
+          target="_blank"
+          className="flex flex-row items-center justify-center gap-2 pb-10"
+        >
+          <div className="text-xl text-zinc-400">
+            For more projects please visit my github page
+          </div>
+          <Image src={github} alt="github" height="20" width="20" />
+        </Link>
       </div>
     </div>
   )
