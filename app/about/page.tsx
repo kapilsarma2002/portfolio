7434bcbeea16bd8941c98d7ae8aae575c5d779a8
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-07-15 18:19:57 +0530

    final

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,17 @@
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 
 const About = () => {
   return (
     <div className="h-screen w-screen">
       <Navbar />
-      <div className="h-[calc(100vh-4rem)] w-screen flex flex-row justify-between gap-2 items-center">
+      <div className="h-[calc(100vh-4rem)] w-screen flex flex-row justify-between gap-1 items-center">
         <div className="h-full w-11/12 flex justify-center items-center">
-          <Image src='/code1.jpg' height='800' width='800' alt='coding image' />
+          <Image src="/code1.jpg" height="1000" width="1200" alt="coding image" />
         </div>
         <div className="h-full w-full ">
           <div className="h-full w-full text-slate text-2xl text-zinc-400">
-            <ul className="h-full w-full flex flex-col gap-3 items-left px-[200px] justify-center">
+            <ul className="h-full w-full flex flex-col gap-3 items-left pr-[200px] justify-center">
               <li>My name is Kapil Sarma</li>
               <li>I am from Kakinada, India</li>
               <li>I like to code</li>
@@ -20,9 +20,10 @@ const About = () => {
               <li>
                 Some of the technologies I have worked with are:
                 <div className="text-2xl text-zinc-500 my-2">
-                  React, Next, Angular, Node, C++ (for problem solving), Python, SQL, Java
+                  React, Next, Angular, Node, C++ (for problem solving), Python,
+                  SQL, Java
                 </div>
-              </li>            
+              </li>
             </ul>
           </div>
         </div>
@@ -31,4 +32,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default About
